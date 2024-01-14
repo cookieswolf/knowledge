c7d6a157e02590df3443b821f453ce52ffa93307
cookieswolf <schangjiang@163.com>
2024-01-14 13:27:52 +0800

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -1,5 +1,3 @@
- 
-
 from langchain.llms.base import LLM
 from typing import Any, List, Optional
 from langchain.callbacks.manager import CallbackManagerForLLMRun
@@ -15,26 +13,29 @@ class InternLM_LLM(LLM):
         # model_path: InternLM 模型路径
         # 从本地初始化模型
         super().__init__()
-        print("loading model...")
-        print(model_path)
+        print("正在从本地加载模型...")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
         self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
-        #self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16)
         self.model = self.model.eval()
-        print("loaded model")
+        print("完成本地模型的加载")
 
     def _call(self, prompt : str, stop: Optional[List[str]] = None,
                 run_manager: Optional[CallbackManagerForLLMRun] = None,
                 **kwargs: Any):
         # 重写调用函数
-        response, history = self.model.chat(self.tokenizer, prompt , history=[])
+        system_prompt = """You are an AI assistant whose name is InternLM (书生·浦语).
+                        - InternLM (书生·浦语) is a conversational language model that is developed by Shanghai AI Laboratory (上海人工智能实验室). It is designed to be helpful, honest, and harmless.
+                        - InternLM (书生·浦语) can understand and communicate fluently in the language chosen by the user such as English and 中文.
+                        """
+        messages = [(system_prompt, '')]
+        response, history = self.model.chat(self.tokenizer, prompt , history=messages)
         return response
         
     @property
     def _llm_type(self) -> str:
         return "InternLM"
     
-if __name__ == "main":
+if __name__ == "__main__":
     # 测试代码
     llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
     print(llm.predict("你是谁"))
